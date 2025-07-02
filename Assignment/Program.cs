91a2c7f53edfc9d93d8f705d4c4efc62df7ab2ce
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-07-02 19:41:03 +0300

    Question_1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Assignment
+﻿using System.ComponentModel;
+
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //1 - Write a program that takes a number from the user then print yes if that
+            //number can be divided by 3 and 4 otherwise print no.
+            bool Isparse;
+
+            int num;
+
+            do
+            {
+                Console.WriteLine("please enter the number ");
+                Isparse=int.TryParse(Console.ReadLine(), out num);
+
+
+            } while (!Isparse);
+
+            if (num % 4 ==0 && num % 3 == 0)
+            {
+                Console.WriteLine("yes");
+            }else
+            {
+                Console.WriteLine("no");
+            }
+
+            #endregion
         }
     }
 }
