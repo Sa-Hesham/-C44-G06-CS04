b1571bd297cd8e5e100b504686b1b5ecb0cb4937
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-07-02 20:24:57 +0300

    Question_3

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
 using System.Threading.Channels;
 using System.Transactions;
+using System.Xml.Linq;
 
 namespace Assignment
 {
@@ -55,7 +58,101 @@ namespace Assignment
 
             //Console.WriteLine(message);
 
-        #endregion
+            #endregion
+
+
+            #region Q3
+
+            //3 - Write a program that takes 3 integers from the user then prints the max
+            //element and the min element.
+
+            Console.WriteLine("enter number 1 ");
+            int.TryParse(Console.ReadLine(), out int num1);
+            Console.WriteLine("enter number 2 ");
+            int.TryParse(Console.ReadLine(), out int num2);
+            Console.WriteLine("enter number 3 ");
+            int.TryParse(Console.ReadLine(), out int num3);
+
+            //// ======== first way ===========
+            //int max = 0;
+            //int min = 0;
+            //if (num1 > num2 && num1 >num3)
+            //{
+            //    max = num1;
+            //    if (num2 >num3)
+            //    {
+            //        min = num3;
+            //    }else
+            //    {
+            //        min = num2;
+            //    }
+
+            //}else if (num2 >num1 && num2 > num3)
+            //{
+            //    max = num2;
+            //    if (num1 > num3)
+            //    {
+            //        min = num3;
+            //    }
+            //    else
+            //    {
+            //        min = num1;
+            //    }
+
+            //}else
+            //{
+            //    max = num3;
+            //    if (num1 > num2)
+            //        min = num2;
+            //    else
+            //        min = num1;
+            //}
+
+            //Console.WriteLine($"max number is {max} ,,  min number is :: {min}");
+
+
+
+
+            //=============Another Way===================== 
+           int Maxnum = num1;
+            int Minnum = num1;
+
+            if (num2 > Maxnum)
+                Maxnum = num2;
+            if (num3 > Maxnum)
+                Maxnum = num3;
+
+
+
+            if (num2 < Maxnum)
+                Minnum = num2;
+            if (num3 < Maxnum)
+                Minnum = num3;
+
+            Console.WriteLine($"max number is {Maxnum} ,,  min number is :: {Minnum}");
+
+
+
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
 
     }
