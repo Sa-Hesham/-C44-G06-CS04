de2b4ab367f4c79e6f5c6734d97078eba038d7b0
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-07-02 19:50:04 +0300

    Question_2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Channels;
+using System.Transactions;
 
 namespace Assignment
 {
@@ -9,27 +12,51 @@ namespace Assignment
             #region Q1
             //1 - Write a program that takes a number from the user then print yes if that
             //number can be divided by 3 and 4 otherwise print no.
-            bool Isparse;
+            //bool Isparse;
 
-            int num;
+            //int num;
 
-            do
-            {
-                Console.WriteLine("please enter the number ");
-                Isparse=int.TryParse(Console.ReadLine(), out num);
+            //do
+            //{
+            //    Console.WriteLine("please enter the number ");
+            //    Isparse=int.TryParse(Console.ReadLine(), out num);
 
 
-            } while (!Isparse);
+            //} while (!Isparse);
 
-            if (num % 4 ==0 && num % 3 == 0)
-            {
-                Console.WriteLine("yes");
-            }else
-            {
-                Console.WriteLine("no");
-            }
+            //if (num % 4 ==0 && num % 3 == 0)
+            //{
+            //    Console.WriteLine("yes");
+            //}else
+            //{
+            //    Console.WriteLine("no");
+            //}
 
             #endregion
+
+
+
+            #region Q2
+
+            ////    Write a program that allows the user to insert an integer then print
+            ////negative if it is negative number otherwise print positive.
+            //Console.WriteLine("Enter the number ");
+            //int.TryParse(Console.ReadLine(), out int x);
+            //string message = x switch
+            //{
+
+            //    > 0 => "positive",
+            //    < 0 => "negative",
+            //    _ => " it not positive or negative  it is Zero "
+
+
+
+            //};
+
+            //Console.WriteLine(message);
+
+        #endregion
         }
+
     }
 }
